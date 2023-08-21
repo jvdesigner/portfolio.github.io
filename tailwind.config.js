80bf84b70a1e99e16e15824148724857f08872a5
jvdesigner <142459887+jvdesigner@users.noreply.github.com>
2023-08-21 13:12:21 +0000

    comitando

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,40 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+
+      animation: {
+          fade_in_left: "fade_in_left 1s linear",
+          fade_in_right: "fade_in_right 1s linear",
+        },
+  
+        keyframes: {
+  
+          fade_in_left: {
+            "0%": {
+              opacity:"0",
+              transform:"translatex(-200px)",
+            },
+            "100%": {
+              opacity:"1",
+              transform:"translatex(0px)",
+            },
+          },
+  
+           fade_in_right: {
+            "0%": {
+              opacity:"0",
+              transform:"translatex(200px)",
+            },
+            "100%": {
+              opacity:"1",
+              transform:"translatex(0px)",
+            },
+          },
+  
+        },
+       
+      },
   },
   plugins: [],
 }
